97c2ea6e3c148080d098019d2af4e68c314842c5
konkarah <KONKARAHDEH@GMAIL.COM>
2023-04-30 14:51:43 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import { useState, useEffect } from 'react'
+import AddTask from "./components/AddTask";
 
 function App() {
+  const [showAddTasks, setShowAddTasks] = useState(false)
+  const [tasks, setTasks] = useState([])
+
+  useEffect (()=> {
+    const getTasks = async () =>{
+      const tasksfromserver = await fetchTasks()
+      setTasks(tasksfromserver)
+    }
+
+    getTasks()
+  }, [])
+
+  //fetch tasks
+  const fetchTasks = async () => {
+    const res = await fetch ('http://localhost:5000/tasks')
+    const data = await res.json()
+
+      return data
+    console.log(data)
+  }
+
+//Add Task 
+const addTask = (task) => {
+  const id = Math.floor(Math.random() * 10000) + 1
+  const newTask = {id, ...task}
+  setTasks([...tasks, newTask])
+}
+
+//delete task
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task) => task.id !==id))
+}
+
+//toggle reminder
+const togglereminder = (id) => {
+  setTasks(tasks.map((task) => task.id === id
+  ? { ...task, reminder : ! task.reminder}: task))
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header onAdd={() => setShowAddTasks(!showAddTasks)} showAdd={showAddTasks}/>
+      {showAddTasks && <AddTask onAdd = {addTask} />}
+      {tasks.length > 0 ? 
+      <Tasks tasks={tasks } onDelete={deleteTask} ontoggle = {togglereminder} /> : 'No Tasks to Show'}
     </div>
   );
 }
+
 
 export default App;
---
+++ src/components/Button.js
@@ -0,0 +1,8 @@
+const Button = ({color,text,onClick}) => {
+ 
+    return <button 
+    onClick={onClick}
+    className='btn'
+    style = {{backgroundColor:color}}>{text}</button>
+}
+export default Button
